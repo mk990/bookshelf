a667580d900ec7a70eab37a7dd74532a4b53dd7f
Emad <141444183+Emadshirzad@users.noreply.github.com>
2024-05-03 18:46:00 +0330

    fixed sqlite not found error and server error with artisan

--- routes/console.php
+++ routes/console.php
@@ -6,3 +6,14 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('sqlite:create', function () {
+    $database = database_path('database.sqlite');
+    file_put_contents($database, '');
+    $this->info("SQLite database file created at {$database}");
+});
+
+Artisan::command('env:create', function () {
+    $env = file_put_contents('.env', '');
+    $this->info(".env database file created at {$env}");
+});
